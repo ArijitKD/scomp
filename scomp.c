913d06f6fd733f344c3e86848e618fb89bf9686f
ArijitKD <arijitkdgit.official@gmail.com>
2025-03-01 14:08:34 +0530

    Refactor code

--- scomp.c
+++ scomp.c
@@ -14,16 +14,16 @@
 typedef unsigned char bool;
 
 struct compressed_data {
-        char* data;
-        size_t* decode_seq;
+        char *data;
+        size_t *decode_seq;
 };
 
-struct compressed_data* compress(char* data)
+struct compressed_data *compress(char *data)
 {
         if (!data)
                 return NULL;
 
-        struct compressed_data* cd = malloc(sizeof(*cd));
+        struct compressed_data *cd = malloc(sizeof(*cd));
         if (!cd)
                 return NULL;
         
@@ -38,7 +38,10 @@ struct compressed_data* compress(char* data)
                 return NULL;
         }
         
-        size_t i = 0, j = 0, count;
+        size_t i = 0;
+        size_t j = 0;
+        size_t count;
+
         while (i < datlen) {
                 count = 0;
                 
@@ -50,8 +53,8 @@ struct compressed_data* compress(char* data)
                 ++j; 
         }
         
-        char* tempdata   = realloc(cd->data, j + 1 * sizeof(char));
-        size_t* tempdseq = realloc(cd->decode_seq, j * sizeof(size_t));
+        char *tempdata   = realloc(cd->data, j + 1 * sizeof(char));
+        size_t *tempdseq = realloc(cd->decode_seq, j * sizeof(size_t));
         
         if (tempdata)
                 cd->data = tempdata;
@@ -62,7 +65,7 @@ struct compressed_data* compress(char* data)
         return cd;   
 }
 
-char* decompress(struct compressed_data* cd)
+char *decompress(struct compressed_data *cd)
 {
         if (!cd || !cd->data || !cd->decode_seq)
                 return NULL;
@@ -73,7 +76,7 @@ char* decompress(struct compressed_data* cd)
         for (size_t i = 0; i < compresslen; ++i)
                 datlen += cd->decode_seq[i];
 
-        char* unpacked_data = calloc(nullpad(datlen), sizeof(char));
+        char *unpacked_data = calloc(nullpad(datlen), sizeof(char));
         
         if (!unpacked_data)
                 return NULL;
@@ -94,7 +97,7 @@ char* decompress(struct compressed_data* cd)
         return unpacked_data;        
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
         bool unpack           = 0;
         bool memalloc_failure = 0;
@@ -113,7 +116,7 @@ int main(int argc, char* argv[])
         struct compressed_data *cd = NULL;
         
         if (!unpack) {
-                char* data = argv[NO_UNPACK_STR_INDEX];
+                char *data = argv[NO_UNPACK_STR_INDEX];
                         
                 cd = compress(data);
 
@@ -151,10 +154,10 @@ int main(int argc, char* argv[])
                 if (!cd)
                         goto memerr;
                 
-                size_t no_of_unique_char  = argc-FIRST_DSEQ_ELEM_INDEX;
+                size_t no_of_unique_char  = argc - FIRST_DSEQ_ELEM_INDEX;
                 cd->data = argv[UNPACK_STRING_INDEX];
                  
-                cd->decode_seq = malloc(no_of_unique_char*sizeof(size_t));
+                cd->decode_seq = malloc(no_of_unique_char * sizeof(size_t));
                 
                 if (!cd->decode_seq) {
                         memalloc_failure = 1;
@@ -162,18 +165,18 @@ int main(int argc, char* argv[])
                 }
                 
                 for (size_t i = 0; i < no_of_unique_char &&
-                i+FIRST_DSEQ_ELEM_INDEX < (size_t)argc; ++i)
-                        cd->decode_seq[i] = atoi(argv[i+FIRST_DSEQ_ELEM_INDEX]);
+                i + FIRST_DSEQ_ELEM_INDEX < (size_t)argc; ++i)
+                        cd->decode_seq[i] = atoi(argv[i + FIRST_DSEQ_ELEM_INDEX]);
 
                 printf ("Compressed string: \"%s\"\n", cd->data);
                 printf ("Decode sequence: {");
                 
-                for (size_t i = 0; i < no_of_unique_char-1; ++i)
+                for (size_t i = 0; i < no_of_unique_char - 1; ++i)
                         printf ("%zu, ", cd->decode_seq[i]);
 
-                printf ("%zu}\n", cd->decode_seq[no_of_unique_char-1]);
+                printf ("%zu}\n", cd->decode_seq[no_of_unique_char - 1]);
                 
-                char* unpacked_data = decompress(cd);
+                char *unpacked_data = decompress(cd);
 
                 if (!unpacked_data)
                         goto memerr;
